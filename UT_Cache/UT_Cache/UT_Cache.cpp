838dccb8c0fe056f7e24bb2a473c35e91fffd9fd
WernerS <ws.github@gisbw.de>
2021-03-02 20:40:11 +0100

    WS::Cache und Spezialisierungen WS::CacheSingleThread WS::CacheDuration

--- UT_Cache/UT_Cache/UT_Cache.cpp
+++ UT_Cache/UT_Cache/UT_Cache.cpp
@@ -11,6 +11,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #include <optional>
 
 #include <string>
+#include <chrono>
 
 namespace WS
 {
@@ -20,17 +21,47 @@ namespace WS
 		void lock(){ }
 		void unlock(){ }
 	};
-	template<typename key_type,typename value_type, typename mutex_type=std::mutex> struct Cache
+	template<typename key_type,int duration_value=0,typename duration_type=std::chrono::milliseconds>struct duration_key
+	{
+		using key_t = key_type;
+		using duration_t = duration_type;
+		decltype(std::chrono::system_clock::now()+duration_t{duration_value}) valid_till;
+		key_t key;
+
+		duration_key()=delete;
+		duration_key( key_t key) : key(std::move(key)), valid_till(std::chrono::system_clock::now() + valid_duration()){}
+
+		bool operator<( duration_key const & r ) const {return  this->key<r.key;}
+		bool is_valid() const {return std::chrono::system_clock::now() <= this->valid_till;} 
+		bool is_invalid() const {return !is_valid();}
+
+		static constexpr duration_t valid_duration(){return duration_t{duration_value};}
+	};
+	template<typename key_type>struct duration_key<typename key_type,0>
+	{
+		using key_t = key_type;
+		key_t key;
+
+		duration_key()=delete;
+		duration_key( key_t const & key) : key(key){}
+
+		bool operator<( duration_key const & r ) const {return  this->key < r.key;}
+		bool is_valid() const {return true;} 
+		bool is_invalid() const {return !is_valid();}
+	};
+	template<typename key_type,typename value_type, typename mutex_type=std::mutex,int duration_value=0,typename duration_type=std::chrono::milliseconds> struct Cache
 	{
 		using key_t				= key_type;
+		using duration_key_t	= duration_key<key_t,duration_value,duration_type>;
 		using value_t			= value_type;
 		using mutex_t			= mutex_type;
-		using data_t			= std::map<key_t,value_t>;
+		using data_t			= std::map<duration_key_t,value_t>;
 
 		mutex_t			mutex;
 		data_t			data;
 		Cache(){}
 
+		//hält lock, liefert iterator
 		auto get_locked_item( key_t const & key )
 		{
 			struct r_t: WS::compare_bool
@@ -42,30 +73,42 @@ namespace WS
 			private:
 				bool to_bool() const override{return locked.is_locked();}
 			}ret_value{WS::lock_guard{this->mutex}};
-			ret_value.iter = this->data.find(key);
-			
+			ret_value.iter = this->data.find(duration_key_t{key});
+
 			if( ret_value.iter==data.end() )
 				ret_value.locked.unlock();
+			else if( ret_value.iter->first.is_invalid() )
+			{
+				this->data.erase(ret_value.iter);
+				ret_value.locked.unlock();
+			}
 
 			return ret_value;
 		}
+		//ohne bleibenden lock als kopie
 		std::optional<value_type>	Get( key_t const & key )
 		{
-			auto item = get_locked_item( key );
-			if( item )
+			if( auto item = get_locked_item( key ) )
 				return {item.iter->second};
 			return {};
 		}
+		//wie get_locked_item. hält return-wert lock und liefert iterator
 		auto Set( key_t const & key, value_t value )
 		{
 			decltype(get_locked_item( key )) ret_value{ WS::lock_guard{this->mutex} };
-			ret_value.iter = this->data.insert_or_assign(key, std::move(value)).first;
+			ret_value.iter = this->data.insert_or_assign(duration_key_t{key}, std::move(value)).first;
 			
 			return ret_value;
 		}
-	private:
+
+		void clear(){data.clear();}
+		auto clear( key_t key ){return data.erase(key);}
 	};
-	template<typename key_type,typename value_type> struct CacheSingleThread : Cache<key_type,value_type,null_mutex>
+	template<typename key_type,typename value_type,int duration_value=0,typename duration_type=std::chrono::milliseconds> struct CacheSingleThread : Cache<key_type,value_type,null_mutex,duration_value,duration_type>
+	{
+		using Cache::Cache;
+	};
+	template<typename key_type,typename value_type,int duration_value,typename duration_type=std::chrono::milliseconds, typename mutex_type=std::mutex> struct CacheDuration : Cache<key_type,value_type,mutex_type,duration_value,duration_type>
 	{
 		using Cache::Cache;
 	};
@@ -115,6 +158,112 @@ namespace
 
 namespace UTCache
 {
+	TEST_CLASS(UT_Verstaendnis)
+	{
+		TEST_METHOD(UT_Valid_Data)
+		{
+			using key_t=int;
+			using value_t=std::unique_ptr<int>;
+			std::map<key_t,value_t> map;
+
+			auto v5=map.insert_or_assign(5,std::make_unique<int>(5));
+			Assert::IsTrue( *v5.first->second == 5 );
+			auto v3=map.insert_or_assign(3,std::make_unique<int>(3));
+			Assert::IsTrue( *v5.first->second == 5 );
+			Assert::IsTrue( *v3.first->second == 3 );
+			auto v9=map.insert_or_assign(9,std::make_unique<int>(9));
+			Assert::IsTrue( *v5.first->second == 5 );
+			Assert::IsTrue( *v3.first->second == 3 );
+			Assert::IsTrue( *v9.first->second == 9 );
+			auto v7=map.insert_or_assign(7,std::make_unique<int>(7));
+			Assert::IsTrue( *v5.first->second == 5 );
+			Assert::IsTrue( *v3.first->second == 3 );
+			Assert::IsTrue( *v9.first->second == 9 );
+			Assert::IsTrue( *v7.first->second == 7 );
+			map.erase(3);
+			Assert::IsTrue( *v5.first->second == 5 );
+			//Assert::IsTrue( v3.first );//no way checking if valid
+			Assert::IsTrue( *v9.first->second == 9 );
+			Assert::IsTrue( *v7.first->second == 7 );
+			v3 = map.insert_or_assign(3,std::make_unique<int>(33));
+			Assert::IsTrue( *v5.first->second == 5 );
+			Assert::IsTrue( *v3.first->second == 33 );
+			Assert::IsTrue( *v9.first->second == 9 );
+			Assert::IsTrue( *v7.first->second == 7 );
+			map.insert_or_assign(3,std::make_unique<int>(333));//undefined behavior ? but v3 is valid with correct value
+			Assert::IsTrue( *v5.first->second == 5 );
+			Assert::IsTrue( *v3.first->second == 333 );
+			Assert::IsTrue( *v9.first->second == 9 );
+			Assert::IsTrue( *v7.first->second == 7 );
+			for(key_t i=0;i<1000;++i)
+			{
+				map.insert_or_assign(i,std::make_unique<int>(i));
+				Assert::IsTrue( *v5.first->second == 5 );
+				Assert::IsTrue( *v3.first->second == 333 || *v3.first->second==3);
+				Assert::IsTrue( *v9.first->second == 9 );
+				Assert::IsTrue( *v7.first->second == 7 );
+			}
+
+		}
+		TEST_METHOD(UT_Duration)
+		{
+			using namespace std::literals::chrono_literals;
+
+			std::chrono::milliseconds xx(5);
+
+			auto breakafter = 180ms;
+			std::chrono::time_point<std::chrono::system_clock> starttime = std::chrono::system_clock::now();
+
+			int i;
+			auto stoptime = std::chrono::system_clock::now() + breakafter;
+			for(i=0;;++i)
+			{
+				if( stoptime <= std::chrono::system_clock::now() )
+					break;
+			}
+		}
+		TEST_METHOD(UT_Duration2)
+		{
+			auto till = std::chrono::system_clock::now() + std::chrono::milliseconds{150};
+			while(std::chrono::system_clock::now() <= till )
+			{}
+		}
+		TEST_METHOD(UT_Key)
+		{
+			using namespace std::literals::chrono_literals;
+			auto start_time {std::chrono::system_clock::now()};
+
+			WS::duration_key<int> key_t2{6};
+			WS::duration_key<int,200> key_t1{5};
+			for( auto duration = std::chrono::system_clock::now() - start_time; key_t1.is_valid(); duration = std::chrono::system_clock::now() - start_time)
+			{
+				Assert::IsTrue( duration < 201ms );
+				Assert::IsTrue( key_t2.is_valid() );
+			}
+			auto duration = std::chrono::system_clock::now() - start_time;
+			Assert::IsTrue( 200ms <= duration );
+		}
+		TEST_METHOD(UT_ret_shared_ptr)
+		{
+			struct A
+			{
+				std::shared_ptr<int> data { new int{5} };
+
+				std::shared_ptr<int> &		get_data1(){ return data;}
+				std::shared_ptr<int> const & get_data1() const { return data;}
+				std::shared_ptr<int> &		get_data2(){ return data;}
+				std::shared_ptr<int const> const & get_data2() const { return data;}
+			};
+
+			A a;
+			A const & const_ref_a = a;
+			Assert::IsTrue( *a.get_data1() == 5 );
+			*const_ref_a.get_data1() = 6;//von wegen const. der shared-pointer ist const, nicht dessen inhalt
+			Assert::IsTrue( *a.get_data1() == 6 );
+
+			//*const_ref_a.get_data2() = 7;//error C3892: 'const_ref_a': you cannot assign to a variable that is const
+		}
+	};
 	TEST_CLASS(UTCache)
 	{
 	public:
@@ -149,6 +298,17 @@ namespace UTCache
 
 			auto v1item = cache.get_locked_item(1);
 			Assert::IsFalse( v1item );
+		}
+		TEST_METHOD(cache_int_mystring_valid_75ms)
+		{
+			auto cache = WS::CacheDuration<mykey,mystring,75>{};
+
+			Assert::IsFalse( cache.Get(2).has_value() );
+			cache.Set(3,"welt");
+			auto start_time = std::chrono::system_clock::now();
+			cache.Set(2,"hallo");
+			while( cache.Get(2).has_value() ){}
+			Assert::IsTrue( std::chrono::system_clock::now() - start_time > decltype(cache)::duration_key_t::valid_duration() );
 		}
 	};
 }
