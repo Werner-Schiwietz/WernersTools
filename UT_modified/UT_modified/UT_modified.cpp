cb4ac4659db621c0edb35bd734d377092edd8cb7
WernerS <ws.github@gisbw.de>
2023-09-09 08:09:10 +0200

    release - debug failure

--- UT_modified/UT_modified/UT_modified.cpp
+++ UT_modified/UT_modified/UT_modified.cpp
@@ -4,6 +4,7 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 #include <string>
+#include <tuple>
 
 #include "..\..\headeronly\modified.h"
 
@@ -98,5 +99,35 @@ namespace UT_modified
 			//Assert::IsFalse( WS::modified{v1}	= 50000 );//warning C4309: 'argument': truncation of constant value
 		}
 	#endif
+		TEST_METHOD(UT_assign_compare_reelase_debug_lol)
+		{
+			int i = 1;
+			bool e1 = ((i=2)==i);
+			bool e2 = (i==(i=3));
+			bool e3 = e1 == e2;
+			Assert::IsTrue( e1 == e2 );
+			Assert::IsTrue( ((i=2)==i) == (i==(i=3)) );
+			Assert::IsTrue(e3);
+
+			//parameter werden von rechts nach links auf den stack gelegt. aber nur im debug(ohne optimierung) im release wird immer der selbe wert auf den stack gelegt, lol
+			auto foo = [](int i1,int i2,int i3) 
+			{
+				return std::tuple<int,int,int>{ i1,i2,i3};
+			};
+
+			i=1;
+			auto x1 = foo(++i,++i,++i);
+			auto x2 = foo(i=12,i,i=14);
+		#ifdef _DEBUG
+			Assert::IsTrue( x1 == std::tuple<int,int,int>(4,3,2) );
+			Assert::IsTrue( x2 == std::tuple<int,int,int>(12,14,14) );
+		#else
+			Assert::IsTrue( x1 == std::tuple<int,int,int>(4,4,4) );
+			Assert::IsTrue( x2 == std::tuple<int,int,int>(12,12,12) );
+			//Assert::IsTrue( x2 == std::tuple<int,int,int>(14,14,14) );
+		#endif
+
+		}
 	};
+
 }
