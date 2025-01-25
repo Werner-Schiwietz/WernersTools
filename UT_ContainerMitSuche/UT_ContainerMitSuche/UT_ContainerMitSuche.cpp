d39cc8edf43b6a3738ab29d4f271be5dbcab33ca
WernerS <ws.github@gisbw.de>
2025-01-25 22:39:34 +0100

    containerFastFinf erster Wurf

---
+++ UT_ContainerMitSuche/UT_ContainerMitSuche/UT_ContainerMitSuche.cpp
@@ -0,0 +1,142 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+#include <atlstr.h>
+
+
+//ASSERT ohne dialog
+//VERIFY ohne dialog
+namespace
+{
+    //ASSERT/VERIFY ohne dialog
+    void AssertMsg( char const * file, int line, char const * text)
+    {          
+        CStringA str;
+        str.Format( "%s(%d) ASSERT: %s", file, line, text );
+        Logger::WriteMessage( str.GetString() );
+    }
+    void AssertMsg( char const * file, int line, wchar_t const * text)
+    {          
+        CStringW str;
+        str.Format( L"%S(%d) ASSERT: %s", file, line, text );
+        Logger::WriteMessage( str.GetString() );
+    }
+}
+#define STRINGIFYhelp(x) L#x
+#define STRINGIFY(x) STRINGIFYhelp(x)
+#undef ASSERT
+#ifdef _DEBUG
+#define ASSERT(x) {if(!(x))AssertMsg( __FILE__, __LINE__, STRINGIFY(x) ); }
+#else
+#define ASSERT(x)
+#endif
+//ASSERT ohne dialog
+//VERIFY ohne dialog
+
+#include "..\..\headeronly\containerFastFind.h"
+
+
+
+namespace UTContainerMitSuche
+{
+    TEST_CLASS(UT_ContainerMitSuche)
+    {
+        TEST_METHOD(UT_InsertEasy)
+        {
+            struct Data
+            {
+                int i;
+                std::string s;
+                struct GetKey
+                {
+                    auto const & operator()( Data const & data )//return-wert als referenz ist hier unsinn, ich wollte nur prüfen, ob es geht
+                    {
+                        return data.i;
+                    }
+                };
+            };
+            WS::ContainerMitSuche<std::deque<Data>,Data::GetKey> container;
+
+            //ASSERT(5==6);//schreibt ASSERT ins Test-Fenster
+            container.push_back( Data{5,"5"});//index 0
+            container.push_back( Data{3,"3"});//index 1
+            container.push_back( Data{7,"7"});//index 2
+
+            if( auto index = container.find(4); not index ){}
+            else
+            {
+                Assert::Fail(L"sollte invalid sein");
+            }
+            if( auto index = container.find(4) )
+            {
+                Assert::Fail(L"sollte invalid sein");
+            }
+            if( auto index = container.find(5) )
+            {
+                Assert::IsTrue( container[index].i==5 );
+            }
+            else
+            {
+                Assert::Fail(L"sollte valid sein");
+            }
+
+            Assert::IsTrue( container.valid_index( container.find(5) ) );
+            Assert::IsFalse( (bool)container.find(4) );
+            Assert::IsTrue( (bool)container.find(5) );
+            Assert::IsTrue( container.find(5) == 0 );
+            Assert::IsTrue( container.valid_index( container.find(7) ) );
+            Assert::IsTrue( container.find(7) == 2 );
+            Assert::IsTrue( container.valid_index( container.find(3) ) );
+            Assert::IsTrue( container.find(3) == 1 );
+
+            Assert::IsFalse( container.valid_index( container.find(4) ) );
+            Assert::IsTrue( container.findall(2) == nullptr );
+            Assert::IsTrue( container.findall(3) != nullptr );
+            Assert::IsTrue( container.findall(3)->size()==1 );
+            Assert::IsTrue( container[(container.findall(3)->at(0))].i==3 );
+            Assert::IsTrue( container.findall(4) == nullptr );
+            Assert::IsTrue( container.findall(5) != nullptr );
+            Assert::IsTrue( container.findall(6) == nullptr );
+            Assert::IsTrue( container.findall(7) != nullptr );
+            Assert::IsTrue( container.findall(8) == nullptr );
+            try
+            {
+                Assert::IsTrue( container.at(100).i == 0 );//liefert value_t{};
+                Assert::Fail( L"exception erwartet");
+            }
+            catch(...)
+            {}
+            //Assert::IsTrue( container[100].i == 0 );//out of bound im container
+        }
+        TEST_METHOD(UT_InsertEasy_GetKey_Function)
+        {
+            struct Data
+            {
+                int i;
+                std::string s;
+            };
+            auto fnGetKey = []( Data const & data ) -> auto //const & //man könnte auch eine referenz auf den key-member liefern, bei int ist das sicher nicht nötig
+            {
+                return data.i;
+            };
+
+            auto container = WS::ContainerMitSuche<std::vector<Data>,decltype(fnGetKey),WS::null_mutex>{fnGetKey};
+
+            container.push_back( Data{5,"5"});//index 0
+            container.push_back( Data{3,"3"});//index 1
+            container.push_back( Data{7,"7"});//index 2
+
+
+            Assert::IsTrue( container.valid_index( container.find(5) ) );
+            Assert::IsTrue( container.find(5) == 0 );
+            Assert::IsTrue( container.valid_index( container.find(7) ) );
+            Assert::IsTrue( container.find(7) == 2 );
+            Assert::IsTrue( container.valid_index( container.find(3) ) );
+            Assert::IsTrue( container.find(3) == 1 );
+        }
+
+    };
+}
---
+++ headeronly/containerFastFind.h
@@ -0,0 +1,156 @@
+#pragma once
+///Copyright (c) 2025 Werner Schiwietz Werner.githubpublic(at)gisbw(dot)de
+//Jedem, der eine Kopie dieser Software und der zugehörigen Dokumentationsdateien (die "Software") erhält, wird hiermit kostenlos die Erlaubnis erteilt, 
+//ohne Einschränkung mit der Software zu handeln, einschließlich und ohne Einschränkung der Rechte zur Nutzung, zum Kopieren, Ändern, Zusammenführen, Veröffentlichen, 
+//Verteilen, Unterlizenzieren und/oder Verkaufen von Kopien der Software, und Personen, denen die Software zur Verfügung gestellt wird, dies unter den folgenden Bedingungen zu gestatten:
+//Der obige Urheberrechtshinweis und dieser Genehmigungshinweis müssen in allen Kopien oder wesentlichen Teilen der Software enthalten sein.
+//DIE SOFTWARE WIRD OHNE MÄNGELGEWÄHR UND OHNE JEGLICHE AUSDRÜCKLICHE ODER STILLSCHWEIGENDE GEWÄHRLEISTUNG, EINSCHLIEßLICH, ABER NICHT BESCHRÄNKT AUF
+//DIE GEWÄHRLEISTUNG DER MARKTGÄNGIGKEIT, DER EIGNUNG FÜR EINEN BESTIMMTEN ZWECK UND DER NICHTVERLETZUNG VON RECHTEN DRITTER, ZUR VERFÜGUNG GESTELLT. 
+//DIE AUTOREN ODER URHEBERRECHTSINHABER SIND IN KEINEM FALL HAFTBAR FÜR ANSPRÜCHE, SCHÄDEN ODER ANDERE VERPFLICHTUNGEN, OB IN EINER VERTRAGS- ODER 
+//HAFTUNGSKLAGE, EINER UNERLAUBTEN HANDLUNG ODER ANDERWEITIG, DIE SICH AUS, AUS ODER IN VERBINDUNG MIT DER SOFTWARE ODER DER NUTZUNG ODER ANDEREN 
+//GESCHÄFTEN MIT DER SOFTWARE ERGEBEN. 
+
+#include <map>
+#include <vector>
+#include <deque>
+#include <mutex>
+
+
+namespace WS
+{
+    //für singlethreaded nichts tuender mutex
+    struct null_mutex
+    {
+        void lock(){ }
+        void unlock(){ }
+    };
+}
+namespace WS
+{
+    /// <summary>
+    /// container-wrapper der einen unsortierenden container verwaltet und per std::map items schnell findet
+    /// container muss wie std::vector die funktionen
+    ///                   size
+    ///                   clear
+    ///                   push_back
+    ///                   operator[]
+    ///                   at
+    /// und traits
+    ///                   value_type
+    ///                   size_type
+    /// bereitstellen
+    /// </summary>
+    /// <typeparam name="container_type">std::vector std::deque</typeparam>
+    /// <typeparam name="pred_getKeyType">funktion(sobjekt) welches aus value_t einen key liefert</typeparam>
+    /// <typeparam name="mutex_type"></typeparam>
+    template<typename container_type,typename pred_getKeyType,typename mutex_type=std::mutex>
+    class ContainerMitSuche
+    {
+    public:
+        using container_t = container_type;
+        using value_t = typename container_type::value_type;
+        using index_t = typename container_type::size_type;
+        using mutex_t = typename mutex_type;
+        //using findkey_t = typename std::remove_cv_t<std::remove_reference_t<decltype(pred_getKeyType{}(std::declval<value_t>()))>>;
+        using findkey_t = typename std::remove_cvref_t<decltype(pred_getKeyType{}(std::declval<value_t>()))>;
+
+        virtual ~ContainerMitSuche(){}
+        ContainerMitSuche(){}
+        ContainerMitSuche( pred_getKeyType fnGetKeyType) : GetKey(fnGetKeyType){}//ctor mit key-funktion, z.b. eine lambda
+
+    public:
+        bool valid_index( index_t index ) const { return index>=0 && index<this->daten.size(); }
+
+        void clear()
+        {
+            auto locked=std::lock_guard(this->mutex);
+            this->daten.clear();
+            this->map.clear();
+        }
+
+        index_t push_back( value_t && v )
+        {
+            auto locked=std::lock_guard(this->mutex);
+            auto index = this->daten.size();
+            this->map[GetKey(v)].push_back(index);
+            this->daten.push_back(std::move(v));
+            return index;
+        }
+        index_t push_back( value_t const & v )
+        {
+            return push_back(value_t{v});
+        }
+
+        decltype(auto) operator[](index_t index)
+        {
+            auto locked=std::lock_guard(this->mutex);
+            ASSERT(valid_index(index));
+            return this->daten[index];//?gefährlich ref auf array ohne lock zurückliefern? ggf decltype(auto) zu auto ändern
+        }
+        decltype(auto) operator[](index_t index) const
+        {
+            auto locked=std::lock_guard(this->mutex);
+            ASSERT(valid_index(index));
+            return this->daten[index];//?gefährlich ref auf array ohne lock zurückliefern? ggf decltype(auto) zu auto ändern
+        }
+        decltype(auto) at(index_t index)
+        {
+            auto locked=std::lock_guard(this->mutex);
+            ASSERT(valid_index(index));
+            return this->daten.at(index);//?gefährlich ref auf array ohne lock zurückliefern? ggf decltype(auto) zu auto ändern
+        }
+        decltype(auto) at(index_t index) const
+        {
+            auto locked=std::lock_guard(this->mutex);
+            ASSERT(valid_index(index));
+            return this->daten.at(index);//?gefährlich ref auf array ohne lock zurückliefern? ggf decltype(auto) zu auto ändern
+        }
+
+        //liefert einen pointer auf std:vector<index_t> mit allen einträgen, die die key-bedingung erfüllen, oder nullptr, wenn es keine ergebnismenge gibt
+        auto findall( findkey_t const & key ) const
+        {
+            auto locked=std::lock_guard(this->mutex);
+            return _findall(key);
+        }
+        //wenn der gelieferte wert ein Valid_index ist, ist der wert mit dem key genau einmal enthalten.
+        struct find_ret_t
+        {
+            enum valid_t {valid,key_not_found,key_ambiguous} eValid;
+            index_t index;
+
+            operator index_t() const{ return index; }
+            explicit operator bool() const { return eValid==valid_t::valid; }   
+        }
+        find( findkey_t const & key ) const
+        {
+            auto locked=std::lock_guard(this->mutex);
+
+            if( auto pAllIndex = _findall(key) )
+                if( pAllIndex->size()==1)
+                {
+                    return find_ret_t(find_ret_t::valid,(*pAllIndex)[0]);
+                }
+                else
+                    return {find_ret_t::key_ambiguous,(*pAllIndex)[0]};
+            return {find_ret_t::key_not_found,index_t(-1)};
+        }
+
+    private:
+        //findall ohne eigenen lock
+        auto _findall( findkey_t const & key ) const
+        {
+            auto iter = this->map.find(key);
+            if( iter != this->map.end() )
+            {
+                return &iter->second;
+            }
+            return (decltype(&iter->second))nullptr;
+        }
+
+        pred_getKeyType GetKey{};//funktion(objekt), liefert den key des value
+        container_t daten;
+        std::map<findkey_t,std::vector<index_t>> map;
+        mutable mutex_t mutex{};
+    };
+}
+
